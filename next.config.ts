79de8328c2f113994c45783b2e48efd3c9dff955
Miguel Angel <miguel.carrillo.m@hotmail.com>
2025-08-31 16:36:02 -0500

    until 11th chapter

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // experimental: { // This is only for experimental purposes PPR
+  //   ppr: 'incremental',
+  // }
 };
 
 export default nextConfig;
